440b400d3715085945ba5808985023d4367e00ac
Krishna Das <dask84779@gmail.com>
2026-06-09 18:26:12 +0545

    updated Time Details

--- script/script.js
+++ script/script.js
@@ -1005,7 +1005,7 @@ function submitExam() {
 }
 
 // ---------------- RESULT TIMER ----------------
-let count = 300; // 10 minutes = 600 seconds
+let count = 120; // 10 minutes = 600 seconds
 let timerInterval = null;
 
 function startResultTimer() {
@@ -1037,7 +1037,7 @@ function startResultTimer() {
     document.getElementById("count").innerText = formattedTime;
 
     // Update progress bar (based on 600 seconds total)
-    const progressPercentage = (count / 600) * 100;
+    const progressPercentage = (count / 120) * 100;
     if (progressFill) {
       progressFill.style.width = `${progressPercentage}%`;
 
